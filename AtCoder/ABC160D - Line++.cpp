ff4a5888aea6b04cb38a6204b946167a8a56aa23
miya789 <miya.nori42395@gmail.com>
2020-05-10 17:36:35 +0900

    Add ABC160A according to #13005538

--- AtCoder/ABC160D - Line++.cpp
+++ AtCoder/ABC160D - Line++.cpp
@@ -11,26 +11,21 @@ template<class T> inline bool chmin(T& a, T b) { if(a > b) {a = b; return true; 
 const long long INF = 1LL << 60;
 
 // Input
-int K, N;
-int A[MAX_N];
+int N, X, Y;
 
 int main() {
-  cin >> K >> N;
-  for (int i = 0; i < N; i++) cin >> A[i];
+  cin >> N >> X >> Y;
+  vector<int> ans(N,0);
+  X--;
+  Y--;
 
-  int max_diff = 0;
   for (int i = 0; i < N; i++) {
-    for (int j = i+1; j < N; j++) {
-      int judge = (A[i] - K/2) * (A[j] - K/2);
-      if (judge >= 0) {
-        // cout << A[i] << ",\t" << A[j] << ",\t" << judge << ",\t" << abs(A[i] - A[j]) << endl;
-        chmax(max_diff, abs(A[i] - A[j]));
-      } else {
-        // cout << A[i] << ",\t" << A[j] << ",\t" << judge << ",\t" << K - abs(A[i] - A[j]) << endl;
-        chmax(max_diff, K - abs(A[i] - A[j]));
-      }
+    for (int j = i + 1; j < N; j++) {
+      int d = min(min(abs(j-i),abs(X-i) + 1 + abs(j-Y)),abs(Y-i) + 1 + abs(j-X));
+      ans.at(d)++;
     }
   }
-
-  cout << K - max_diff << endl;
+  for (int d = 1; d < N; d++) {
+    cout << ans.at(d) << endl;
+  }
 }
