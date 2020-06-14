3f586f497df9c550bbe59cc8563a83a6170088b2
miya789 <miya.nori42395@gmail.com>
2020-06-14 21:29:47 +0900

    Add ABC170A as AC

---
+++ AtCoder/ABC170A - Five Variables.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main() {
+  vector<int> x(5);
+  for (int i = 0; i < 5; i++) cin >> x.at(i);
+
+  for (int i = 0; i < 5; i++) {
+    if (x[i] == 0) cout << i+1 << endl;
+  }
+  return 0;
+}
