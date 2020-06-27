f6152e5dd9d293aaf26f5fe325485b48e3f419f0
miya789 <miya.nori42395@gmail.com>
2020-06-27 21:03:24 +0900

    Add ABC172A as AC

---
+++ AtCoder/ABC172A - Calc.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define MAX_N 100000
+
+int main() {
+  int a;
+  cin >> a;
+
+  cout << a + pow(a,2) + pow(a,3) << endl;
+  return 0;
+}
