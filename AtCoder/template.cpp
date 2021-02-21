2bfc89677b48785b5caa57a8ed08f037d3a94de4
miya789 <miya.nori42395@gmail.com>
2021-02-21 16:21:15 +0900

    Add temlate for c++
    ref: https://atcoder.jp/contests/abc192/submissions/20345441

---
+++ AtCoder/template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#define REP(i,a,b) for (int i=a; i<b; i++)
+#define ALL(v) (v).begin(), (v).end()
+#define FORE(i,a) for(auto &i:a)
+using namespace std;
+typedef long long ll;
+const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (a>b) { a=b; return 1; } return 0; }
+//#pragma GCC optimize ("-O3")
+void _main(); int main() {cin.tie(0); ios::sync_with_stdio(false); _main(); }
+
+void _main() {
+  ll N, M;
+  cin >> N >> M;
+
+  ll ans = 0;
+  cout << ans << endl;
+}
