e2ddf98910d13c036caab4aeb2ce6788a8f83f47
miya789 <miya.nori42395@gmail.com>
2021-03-02 18:57:10 +0900

    Fix ABC073C simpler as AC with set

--- AtCoder/ABC073C - Write and Erase.cpp
+++ AtCoder/ABC073C - Write and Erase.cpp
@@ -4,22 +4,18 @@ using namespace std;
 int main() {
   int N;
   cin >> N;
-  vector<int> A(N);
-  for (int i = 0; i < N; i++) cin >> A[i];
-  sort(A.begin(), A.end());
-
-  int tmp = -1;
-  int ans = 0;
+  set<int> s;
   for (int i = 0; i < N; i++) {
-    if (tmp != A[i]) {
-      tmp = A[i];
-      ans++;
+    int a;
+    cin >> a;
+
+    if (s.count(a)) {
+      s.erase(a);
     } else {
-      ans--;
-      tmp = -1;
+      s.insert(a);
     }
   }
 
-  cout << ans << endl;
+  cout << s.size() << endl;
   return 0;
 }
